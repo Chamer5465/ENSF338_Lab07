4a0af6ea4ffeaa3821c805ed41e243152d092b34
Chamer5465 <chasemack@shaw.ca>
2025-03-23 14:20:21 -0600

    Ex2 complete

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -11,7 +11,38 @@ class Node:
         self.height = 1
         self.balance = 0
         
-def non_AVL_insert(data, root=None):
+def insert(data, root=None):
+    current = root
+    parent = None
+    pivot = None
+    while current is not None:
+        parent = current
+        if current.balance != 0:
+            pivot = current
+        if data <= current.data:
+            current = current.left
+        else:
+            current = current.right
+    if root is None:
+        root = Node(data)
+    elif data <= parent.data:
+        parent.left = Node(data, parent)
+    else:
+        parent.right = Node(data, parent)
+    if pivot == None:
+        print('Case #1: Pivot not detected.')
+    elif (pivot.balance == 1 and data < pivot.data) or (pivot.balance == -1 and data > pivot.data):
+        print('Case #2: A pivot exists, and a node was added to the shorter subtree.')
+    else:
+        print('Case #3 not supported.')
+    while parent is not None:
+        current = parent
+        parent = current.parent
+        current.height = 1 + max(getHeight(current.left), getHeight(current.right))
+        current.balance = getHeight(current.right) - getHeight(current.left)
+        
+
+def non_avl_insert(data, root=None):
     current = root
     parent = None
     while current is not None:
@@ -32,54 +63,13 @@ def non_AVL_insert(data, root=None):
         
         parent.balance = getHeight(parent.right) - getHeight(parent.left)
         
-        if (parent.balance == 1 and data < parent.data) or (parent.balance == -1 and data > parent.data):
-            print('Case #2: A pivot exists, and a node was added to the shorter subtree.')
-            current = parent
-            parent = current.parent
-            continue
-        elif (parent.balance != 0):
-            print('#Case #3 not supported.')
-            current = parent
-            parent = current.parent
-            continue
-        
         current = parent
         parent = current.parent
-        
-        print('Case #1: Pivot not detected.')
-        
-
-def insert(data, root=None):
-    if root == None:
-        return Node(data)
-    
-    if data < root.data:
-        root.left = insert(data, root.left)
-    elif data > root.data:
-        root.right = insert(data, root.right)
-        
-    root.height = 1 + max(getHeight(root.right), getHeight(root.left))
-    
-    root.balance = getHeight(root.right) - getHeight(root.left)
-    
-    if (root.balance == 1 and data < root.data) or (root.balance == -1 and data > root.data):
-        print('Case #2: A pivot exists, and a node was added to the shorter subtree.')
-        return root
-    elif (root.balance != 0):
-        print('#Case #3 not supported.')
-        return root
-    
-    print('Case #1: Pivot not detected.')
-    return root
-        
     
 def getHeight(root):
     if root == None:
         return 0
     return root.height
-    
-        
-    
     
 def search(data, root):
     current = root
@@ -95,12 +85,34 @@ def search(data, root):
 
 
 def main():
-    theList = [x for x in range(10)]
-    random.shuffle(theList)
+    theList = [5, 3, 8, 2, 7, 11]
     root = Node(theList[0])
     for e in theList[1:]:
-        non_AVL_insert(e, root)
-    print('lets go')
+        non_avl_insert(e, root)
+    insert(10, root)
+    insert(4, root)
+    insert(9, root)
+    theList = [10, 6, 16, 4, 14, 22]
+    root = Node(theList[0])
+    for e in theList[1:]:
+        non_avl_insert(e, root)
+    insert(20, root)
+    insert(8, root)
+    insert(18, root)
+    theList = [15, 9, 24, 6, 21, 33]
+    root = Node(theList[0])
+    for e in theList[1:]:
+        non_avl_insert(e, root)
+    insert(30, root)
+    insert(12, root)
+    insert(27, root)
+    theList = [35, 21, 56, 14, 49, 77]
+    root = Node(theList[0])
+    for e in theList[1:]:
+        non_avl_insert(e, root)
+    insert(70, root)
+    insert(28, root)
+    insert(75, root)
     
 
 if __name__ == '__main__':
